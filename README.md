6fbdb822a3f8980965be42a2645f87d462f37574
david <noemail>
2021-07-20 13:42:59 +0700

    Add welcome animation

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+<a href="https://www.freepik.com/vectors/background">Background vector created by rawpixel.com - www.freepik.com</a>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className="h-64 bg-white">
-            
-        </div>
+        <footer className="h-36 bg-white font-semibold grid grid-cols-2 items-center">
+            <div className="ml-5">
+                <h4 className="text-xl font-semibold mb-3 mx-4">Contact</h4>
+                <a href="https://github.com/davidlidebrandt" className="text-4xl mx-6 hover:text-green-100 cursor-pointer"><i className="fab fa-github-square transform hover:scale-150"></i> </a>
+                <a href="https://www.linkedin.com/in/david-lidebrandt-5842411b6/" className="text-4xl mx-6 hover:text-green-100 cursor-pointer"><i className="fab fa-linkedin transform hover:scale-150"></i></a>
+                <button className="text-4xl mx-6 hover:text-green-100"> <i className="fas fa-envelope-square transform hover:scale-150"></i></button>
+            </div>
+            <div className="">
+                <p className="">Site created by David Lidebrandt</p>
+                <a href="https://www.freepik.com/vectors/background">Background vector created by rawpixel.com - www.freepik.com</a>
+            </div>
+        </footer>
     )
 }
